e807977f80f5f80d438520b93c5834dcd5db0e71
Tim Dysinger <tim@dysinger.net>
2010-10-29 10:30:17 -0400

    The project doesn't require clojure-contrib

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
   :description "Arrows are a generalization of monads. This libary assists
                in the defintion of arrows.
                http://www.cse.chalmers.se/~rjmh/Papers/arrows.pdf"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
 (defproject arrows "1.2.0"
+  :dependencies [[org.clojure/clojure "1.2.0"]])
