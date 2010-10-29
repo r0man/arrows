3d1e4c83595943993b10cee0ad6339805456980c
Tim Dysinger <tim@dysinger.net>
2010-10-29 10:31:19 -0400

    Short and simple description

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
-  :description "Arrows are a generalization of monads. This libary assists
-               in the defintion of arrows.
-               http://www.cse.chalmers.se/~rjmh/Papers/arrows.pdf"
 (defproject arrows "1.2.0"
+  :description "Arrows: A Generalization of Monads."
   :dependencies [[org.clojure/clojure "1.2.0"]])
