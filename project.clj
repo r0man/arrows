526e76787143a043500944712333ae0ddf473f1b
Roman <roman@burningswell.com>
2011-09-24 14:09:51 +0200

    Switched to Clojure 1.3.0.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject arrows "1.2.0"
   :description "Arrows: A Generalization of Monads."
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.macro "0.1.1"]])
